e2378e0fc01f6aba4b905bd828708ef3009513e9
MihneaSpinu <d.spinu.dd@gmail.com>
2025-10-23 10:25:45 +0200

    Setup, with template

---
+++ src/main/java/p3project/Project.java
@@ -0,0 +1,12 @@
+package p3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project.class, args);
+	}
+}
