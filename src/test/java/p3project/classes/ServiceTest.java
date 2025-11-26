eac12c692e4800cf37645ebc3eedb1e1b3c69279
ARCH-Emborg <SH47YI@student.aau.dk>
2025-11-26 13:19:29 +0100

    -Changed the name of a function in service from  isStatus to getStatus -Made tests for Service.java and Type.java

---
+++ src/test/java/p3project/classes/ServiceTest.java
@@ -0,0 +1,26 @@
+package p3project.classes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.Banner;
+
+public class ServiceTest {
+    
+    @Test
+    public void testServiceCreation(){
+        Service service = new Service(ServiceType.Tickets, true, 20);
+
+        service.setAmountOrDuration(10);
+        service.setType(ServiceType.Banner);
+        service.setStatus(false);
+
+        assertEquals(false, service.getStatus());
+        assertEquals(ServiceType.Banner, service.getType());
+        assertEquals(10, service.getAmountOrDuration());
+
+    }
+
+    
+}
