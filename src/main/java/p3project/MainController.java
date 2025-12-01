0b57d45a3aec90c05fae6ccc08e65de2dfe1b113
Jacob580234 <jsand24@student.aau.dk>
2025-12-01 13:52:22 +0100

    -JWT måske -Start på arkiv tjek

--- src/main/java/p3project/MainController.java
+++ src/main/java/p3project/MainController.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
