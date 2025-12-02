ff6753ef9eb4894b5475e0f606981aaa46ba0e14
MihneaSpinu <d.spinu.dd@gmail.com>
2025-12-02 13:44:27 +0100

    login redirect virker
    det er ikke lavet på alle sider

--- src/main/java/p3project/Project.java
+++ src/main/java/p3project/Project.java
@@ -2,9 +2,8 @@ package p3project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class Project {
 
 	public static void main(String[] args) {
